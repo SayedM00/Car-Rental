3d8710ff77c07ed15ef18ac178246cb1adbc2471
SayedM00 <elsayedmoharam000@gmail.com>
2023-12-10 13:04:49 -0800

    First Chalnge

--- src/index.js
+++ src/index.js
@@ -1,42 +1,113 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css"
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Menu />
-      <Footer />
+function Card() {
+  return <div className="cardBody">
+    <Image />
+    <div style={{padding:"20px"}}>
+      <Title />
+      <Info />
+      <div>
+        <Skill skillName="HTML + CSS" />
+      </div>
     </div>
-  );
-}
-
-function Header() {
-  return <h1>Domins Pizza</h1>
-}
-
-function Menu() {
-  return <div>
-    <h6>Pizza Menu</h6>
-    <Pizza />
-    <Pizza />
-    <Pizza />
   </div>
 }
 
-function Pizza() {
-  return (
-    <div>
-      <h1>Pizza Mixed Meat</h1>
-      <p>Ingreditance: flour, mixed-meat, tomato, sault, cheese</p>
-      <img src="pizzas/margherita.jpg" alt="Pizza Mixed Meat" />
-      <hr></hr>
-    </div>
-  );
+function Image() {
+  return <img src="pizzas/focaccia.jpg" alt="pizza"/>
 }
 
-function Footer() {
-  return <footer>{new Date().toLocaleTimeString()} We Currently Open</footer>
+function Title() {
+  return <h3 style={{fontSize:"30px"}}>Sayed Yousef Mohararm</h3>
 }
+
+function Info() {
+  return <p>Front-End Web Developer Front-End Web Developer Front-End Web Developer Front-End Web Developer Front-End Web Developer Front-End Web Developer</p>
+}
+
+function Skill(props) {
+return <span>{props.skillName} {props.icon}</span>
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(<Card />)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import "./index.css"
+
+// function App() {
+//   return (
+//     <div className="container">
+//       <Header />
+//       <Menu />
+//       <Footer />
+//     </div>
+//   );
+// }
+
+// function Header() {
+//   return <h1 className="header">Domins Pizza</h1>
+// }
+
+// function Menu() {
+//   return <div className="menu">
+//     <h6 style={{fontSize: "30px", width:'100%'}}>Pizza Menu</h6>
+//     <Pizza />
+//     <Pizza />
+//     <Pizza />
+//     <Pizza />
+//   </div>
+// }
+
+// function Pizza() {
+//   return (
+//     <div style={{width:"50%"}}>
+//       <h1>Pizza Mixed Meat</h1>
+//       <p>Ingreditance: flour, mixed-meat, tomato, sault, cheese</p>
+//       <img style={{width:"100px"}} src="pizzas/margherita.jpg" alt="Pizza Mixed Meat" />
+//       <hr></hr>
+//     </div>
+//   );
+// }
+
+// function Footer() {
+//   return <footer className="footer" style={{fontSize:"30px", color:"black", marginTop:"20px"}}>{new Date().toLocaleTimeString()} We Currently Open</footer>
+// }
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<App />);
